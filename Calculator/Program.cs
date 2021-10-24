fc0b5d637d34161a1ad6a74637c6a4969161af60
palaksharma292 <p.sharma649@mybvc.ca>
2021-10-24 11:33:12 -0600

    expression extracted

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -4,21 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculator
+namespace Class1
 {
     class Program
     {
+        static ArrayList expression= new ArrayList();
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the expression to evaluate it");
             string input = Console.ReadLine();
             Extract(input);
+            for(int i=0;i<expression.Count;i++)
+            {
+                Console.WriteLine(expression[i]);
+            }
         }
 
         public static void Extract(string expr)
         {
-
+            int num=0;
+            for(int i=0;i<expr.Length;i++)
+            {
+                int digit=0;
+                bool canConvert= int.TryParse(expr.Substring(i,1),out digit);
+                if(canConvert)
+                {
+                    num=num*10+digit;
+                    continue;
+                }
+                else
+                {
+                    if(num!=0)
+                    {
+                         expression.Add(num);
+                    }
+                    
+                    num=0;
+                    expression.Add(expr.Substring(i,1));
+                }
+            }
+            
         }
-    }
     }
 }
