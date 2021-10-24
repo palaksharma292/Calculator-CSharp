573da1a82b109c8484fe317453562d031bfa9dc6
palaksharma292 <p.sharma649@mybvc.ca>
2021-10-24 14:15:43 -0600

    Evaluation Working

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -9,20 +9,32 @@ namespace Class1
     class Program
     {
         static ArrayList expression= new ArrayList();
+        static ArrayList operators= new ArrayList();
+        
+
         static void Main(string[] args)
         {
+            operators.Add("/");
+            operators.Add("*");
+            operators.Add("+");
+            operators.Add("-");
             Console.WriteLine("Enter the expression to evaluate it");
             string input = Console.ReadLine();
             Extract(input);
-            for(int i=0;i<expression.Count;i++)
+            while(expression.Count>1)
             {
-                Console.WriteLine(expression[i]);
+                expression=Evaluate(expression); 
+                for(int j=0;j<expression.Count;j++)
+                    {
+                        Console.Write(expression[j]);
+                    }
+                    Console.WriteLine();
             }
         }
 
         public static void Extract(string expr)
         {
-            int num=0;
+            string num="";
             for(int i=0;i<expr.Length;i++)
             {
                 if(expr.Substring(i,1)==" ")
@@ -31,26 +43,85 @@ namespace Class1
                 }
                 int digit=0;
                 bool canConvert= int.TryParse(expr.Substring(i,1),out digit);
-                if(canConvert)
+                if(canConvert||expr.Substring(i,1)==".")
                 {
-                    num=num*10+digit;
+                    num=num+expr.Substring(i,1);
                     continue;
                 }
                 else
                 {
-                    if(num!=0)
+                    if(num!="")
                     {
-                         expression.Add(num);
+                         expression.Add(double.Parse(num));
                     }
                     
-                    num=0;
+                    num="";
                     expression.Add(expr.Substring(i,1));
                 }
             }
-            if(num!=0)
+            if(num!="")
             {
-                expression.Add(num);
+               expression.Add(double.Parse(num));
             }
+        }
+
+        public static ArrayList Evaluate(ArrayList expression)
+        {
+            double val=0;
+            double n1=0,n2=0;
+            while(expression.Contains("("))
+            {
+                ArrayList section=new ArrayList();
+                int sI=expression.IndexOf("(")+1;
+                while(sI<expression.LastIndexOf(")"))
+                {
+                    section.Add(expression[sI]);
+                    sI++;
+                }
+                int secCount=section.Count;
+                while(section.Count>1)
+                {
+                    section=Evaluate(section);
+                }
+                if(expression[expression.IndexOf("(")-1].GetType()== typeof(double))
+                {
+                    expression.Insert(expression.IndexOf("("),"*");
+                }
+                expression.Insert(expression.IndexOf("("),section[0]);
+                expression.RemoveRange(expression.IndexOf("("),secCount+2);
+            }
+            for(int op=0;op<operators.Count;op++)
+            {
+                if(expression.Contains(operators[op]))
+                {
+                    int i=expression.IndexOf( operators[op] );
+                    if(expression[i-1].GetType()== typeof(double))
+                        n1= double.Parse(expression[i-1].ToString());
+                    if(expression[i+1].GetType()== typeof(double))
+                        n2= double.Parse(expression[i+1].ToString());
+
+                    if(operators[op]=="+")
+                    {
+                        val= n1+n2;
+                    }
+                    else if(operators[op]=="-")
+                    {
+                        val= n1-n2;
+                    }
+                    else if(operators[op]=="*")
+                    {
+                        val= n1*n2;
+                    }
+                    else if(operators[op]=="/")
+                    {
+                        val= n1/n2;
+                    }
+                    expression[i-1]=val;
+                    expression.RemoveRange(i,2);
+                    
+                 }
+            }
+            return expression;
         }
     }
 }
