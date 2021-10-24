3db2a73473a6dc4aa1e0d318c7040033b38d6134
palaksharma292 <p.sharma649@mybvc.ca>
2021-10-24 11:17:25 -0600

    file created

---
+++ Calculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the expression to evaluate it");
+            string input = Console.ReadLine();
+            Extract(input);
+        }
+
+        public static void Extract(string expr)
+        {
+
+        }
+    }
+    }
+}
