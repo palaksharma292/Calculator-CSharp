207500c5067ac4904a4b79baf8d9af674729d293
Palak Sharma <80782744+palaksharma292@users.noreply.github.com>
2024-01-06 22:57:15 -0700

    Update UnitTest1.cs

--- CalculatorTests/UnitTest1.cs
+++ CalculatorTests/UnitTest1.cs
@@ -7,10 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-// SODV2202 Assignment 1 Calculator
-// By: Connor Pittman, Palak Sharma, Oxana Beloken, Parry Sobti
-
-// Test Class and Test Methods provided by Course Instructor
 namespace Class1.Tests
 {
     [TestClass()]
